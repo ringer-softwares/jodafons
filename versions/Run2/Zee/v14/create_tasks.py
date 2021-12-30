c3523778f57640f33a478fbc244ce13ab3adb704
jodafons <jodafons@lps.ufrj.br>
2021-12-30 11:52:20 -0300

    update

---
+++ versions/Run2/Zee/v14/create_tasks.py
@@ -0,0 +1,34 @@
+
+import os
+basepath = os.getcwd()
+
+path = basepath + '/Zee/v14'
+
+# from...
+exec_cmd = "git clone https://github.com/ringer-softwares/jodafons.git && "
+# exec this
+exec_cmd+= "python jodafons/versions/Run2/Zee/v14/job_tuning.py -c %IN -d %DATA -r %REF -v %OUT"
+
+command = """maestro.py task create \
+  -v {PATH} \
+  -t user.jodafons.data17_13TeV.AllPeriods.sgn.probes_lhmedium_EGAM1.bkg.VProbes_EGAM7.GRL_v97.v14.25bins_et{ET}_eta{ETA}.r0 \
+  -c user.jodafons.job_config.Zee_v14.10sorts.5inits \
+  -d user.jodafons.data17_13TeV.AllPeriods.sgn.probes_lhvloose_EGAM1.bkg.vprobes_vlhvloose_EGAM7.GRL_v97.25bins_et{ET}_eta{ETA}.h5 \
+  --sd "{REF}" \
+  --exec "{EXEC}" \
+  --queue "gpu" \
+  """
+
+try:
+    os.makedirs(path)
+except:
+    pass
+
+for et in range(5):
+    for eta in range(5):
+        ref = "{'%%REF':'user.jodafons.data17_13TeV.AllPeriods.sgn.probes_lhmedium_EGAM1.bkg.vprobes_vlhvloose_EGAM7.GRL_v97.25bins_et%d_eta%d.ref.npz'}"%(et,eta)
+        cmd = command.format(ET=et,ETA=eta,REF=ref,PATH=path,EXEC=exec_cmd)
+        print(cmd)
+        os.system(cmd)
+
+
