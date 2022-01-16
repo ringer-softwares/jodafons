9fdc080ce4016e326d0344741c517db1daaaf3e1
jodafons <jodafons@lps.ufrj.br>
2022-01-15 21:17:01 -0300

    update

--- versions/Run2/Zee/v8/job_reprocess.py
+++ versions/Run2/Zee/v8/job_reprocess.py
@@ -2,39 +2,52 @@
 
 
 
+
 def getPatterns( path, cv, sort):
 
+  pidname = 'el_lhmedium'
+  from kepler.pandas import load_hdf
+  import numpy as np
+  df = load_hdf(path)
+  df = df.loc[ ((df[pidname]==True) & (df.target==1.0)) | ((df.target==0) & (df['el_lhvloose']==False) ) ]
+  col_names= ['trig_L2_cl_ring_%d'%i for i in range(100)]
+  rings = df[col_names].values.astype(np.float32)
+
+
+    
   def norm1( data ):
       norms = np.abs( data.sum(axis=1) )
       norms[norms==0] = 1
       return data/norms[:,None]
+    
+  target = df['target'].values.astype(np.int16)
 
-  from Gaugi import load
-  import numpy as np
-  d = load(path)
-  data = norm1(d['data'][:,1:101])
-  target = d['target']
+  data = norm1(rings)
   target[target!=1]=-1
   splits = [(train_index, val_index) for train_index, val_index in cv.split(data,target)]
-
   x_train = data [ splits[sort][0]]
   y_train = target [ splits[sort][0] ]
   x_val = data [ splits[sort][1]]
   y_val = target [ splits[sort][1] ]
 
-  return x_train, x_val, y_train, y_val, splits
+
+  avgmu = df.avgmu.values
+  avgmu_train = avgmu[splits[sort][0]]
+  avgmu_val = avgmu[splits[sort][1]]
+
+  return x_train, x_val, y_train, y_val, avgmu_train, avgmu_val, splits
 
 
 
 import argparse
-import sys,os
+import sys,os,traceback
 
 
 parser = argparse.ArgumentParser(description = '', add_help = False)
 parser = argparse.ArgumentParser()
 
 
-parser.add_argument('-f','--tunedFile', action='store',
+parser.add_argument('-t','--tunedFile', action='store',
         dest='tunedFile', required = True,
             help = "The tuned file to be reprocess.")
 
@@ -61,16 +74,33 @@ args = parser.parse_args()
 
 try:
 
+
   targets = [
-                ('tight_cutbased' , 'T0HLTElectronT2CaloTight'        ),
-                ('medium_cutbased', 'T0HLTElectronT2CaloMedium'       ),
-                ('loose_cutbased' , 'T0HLTElectronT2CaloLoose'        ),
-                ('vloose_cutbased', 'T0HLTElectronT2CaloVLoose'       ),
+                # cutbased!
+                ('tight' , 'trig_L2_cl_tight'       ),
+                ('medium', 'trig_L2_cl_medium'      ),
+                ('loose' , 'trig_L2_cl_loose'       ),
+                ('vloose', 'trig_L2_cl_vloose'      ),
                 ]
+
+
   
-  
-  from saphyra.decorators import Summary, Reference
-  decorators = [Summary(), Reference(args.refFile, targets)]
+  from saphyra.decorators import Summary, Reference,LinearFit
+
+
+
+  fit = LinearFit(args.refFile, targets, 
+                  xbin_size=0.05, 
+                  ybin_size=0.5, 
+                  ymin=16, 
+                  ymax=60,  
+                  min_avgmu=16, 
+                  max_avgmu=100, 
+                  false_alarm_limit=0.5)
+
+
+
+  decorators = [Summary(), Reference(args.refFile, targets), fit]
   
   from sklearn.model_selection import StratifiedKFold
   from saphyra.applications import BinaryClassificationJob
@@ -91,6 +121,7 @@ try:
 
 except  Exception as e:
   print(e)
+  traceback.print_exc()
 
   # necessary to work on orchestra
   from saphyra import lock_as_failed_job
