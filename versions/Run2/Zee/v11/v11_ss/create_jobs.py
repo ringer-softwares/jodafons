98975e4d5e8a25adfc456cfd35aee8764a1063ec
jodafons <jodafons@lps.ufrj.br>
2022-01-16 19:12:53 -0300

    update

--- versions/Run2/Zee/v11/v11_ss/create_jobs.py
+++ versions/Run2/Zee/v11/v11_ss/create_jobs.py
@@ -1,29 +1,22 @@
 
-
 from saphyra import *
-
-
-def get_model( ):
-  modelCol = []
-  from tensorflow.keras.models import Sequential
-  from tensorflow.keras.layers import Dense, Dropout, Activation, Conv1D, Flatten
-  for n in range(5,5+1):
-    print(n)
-    model = Sequential()
-    model.add(Dense(n, input_shape=(6,), activation='relu', name='dense_layer'))
-    model.add(Dense(1, activation='linear', name='output_for_inference'))
-    model.add(Activation('sigmoid', name='output_for_training'))
-    modelCol.append(model)
-  return modelCol
+import tensorflow as tf
+from tensorflow.keras import layers
 
 
 
-create_jobs( models = get_model(),
+input  = layers.Input(shape=(1,), name = 'Input')
+output  = layers.Dense(1)(input)
+model = tf.keras.Model(input, output, name = "dummy")
+
+
+
+create_jobs( models = [model],
         nInits        = 5,
         nInitsPerJob  = 1,
         sortBounds    = 10,
         nSortsPerJob  = 1,
         nModelsPerJob = 1,
-        outputFolder  = 'job_config.Zee_v11_ss.10sorts.5inits' )
+        outputFolder  = 'job_config.Zee_v11.10sorts.5inits' )
 
 
