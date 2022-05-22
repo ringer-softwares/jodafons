e14cd0ba9d6a06c8a60294e68b233d8b4cb2c182
jodafons <jodafons@lps.ufrj.br>
2022-05-22 14:10:03 -0300

    add build scripts

---
+++ build_workspace.sh
@@ -0,0 +1,10 @@
+
+
+mkdir build
+cd build
+git clone https://gitlab.cern.ch/atlas-trig-egamma/ringer/neuralnet.git
+git clone https://gitlab.cern.ch/atlas-trig-egamma/ringer/kepler.git
+git clone https://gitlab.cern.ch/atlas-trig-egamma/ringer/pybeamer.git
+git clone https://gitlab.cern.ch/atlas-trig-egamma/ringer/rootplotlib.git
+git clone https://gitlab.cern.ch/atlas-trig-egamma/ringer/gaugi.git
+cd ..
---
+++ setup_here.sh
@@ -0,0 +1,9 @@
+
+
+cd build
+cd gaugi && source scripts/setup.sh && cd ..
+cd pybeamer && source scripts/setup.sh && cd ..
+cd kepler && source scripts/setup.sh && cd ..
+cd neuralnet && source scripts/setup.sh && cd ..
+cd rootplotlib && source scripts/setup.sh && cd ..
+cd ..
